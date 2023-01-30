6fc158b93ea9ff99aef1802e24d1e62aeb539fd6
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-30 22:44:23 +0900

    🏮::NullPointException 해결

--- hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/SecurityProperties.kt
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/SecurityProperties.kt
@@ -4,14 +4,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import java.util.*
 
-@ConfigurationProperties("spring.jwt")
+@ConfigurationProperties("jwt")
 @ConstructorBinding
 class SecurityProperties(
+    secretKey: String,
     accessExp: Long,
-    refreshExp: Long,
-    secret: String
+    refreshExp: Long
 ) {
+    val secretKey: String = Base64.getEncoder().encodeToString(secretKey.toByteArray())
     val accessExp: Long = accessExp * 1000
     val refreshExp: Long = refreshExp * 1000
-    val secretKey: String = Base64.getEncoder().encodeToString(secret.toByteArray())
 }
