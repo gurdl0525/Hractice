1f28a087cc169d340be9c70c09a4328048114c31
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-30 19:27:37 +0900

    📌::SecurityConfig

---
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/config/security/SecurityConfig.kt
@@ -0,0 +1,37 @@
+package com.example.global.config.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig(
+    private val objectMapper: ObjectMapper
+) {
+
+    @Bean
+    @Throws(Exception::class)
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf().disable()
+            .formLogin().disable()
+            .cors()
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("*").permitAll()
+
+            .and().build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
