c3f1720e7c1ee5da98c74548bd45f4b2c6918195
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-31 11:08:30 +0900

    📌::RedisRepositoryConfig

---
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/config/redis/RedisRepositoryConfig.kt
@@ -0,0 +1,30 @@
+package com.example.global.config.redis
+
+import com.example.global.persistence.redis.dotenv.RedisProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@EnableRedisRepositories
+@Configuration
+class RedisRepositoryConfig(
+    private val property: RedisProperty
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory{
+        return LettuceConnectionFactory(property.host, property.port)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, String>{
+        val template: RedisTemplate<String, String> = RedisTemplate()
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = StringRedisSerializer()
+        template.setConnectionFactory(redisConnectionFactory())
+        return template
+    }
+}
