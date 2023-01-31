de301c92054be8de2e969e7fbf654f52cd39580d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-01 00:42:08 +0900

    🔧::Refactoring

--- hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/ExpiredTokenException.kt
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/ExpiredTokenException.kt
@@ -4,6 +4,7 @@ import com.example.global.error.BusinessException
 import com.example.global.error.data.GlobalErrorCode
 
 class ExpiredTokenException(): BusinessException(GlobalErrorCode.EXPIRED_TOKEN) {
+
     companion object {
         @JvmField
         val EXCEPTION = ExpiredTokenException()
