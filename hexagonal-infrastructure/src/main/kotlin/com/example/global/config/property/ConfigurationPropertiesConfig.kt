f9e6d61debf15d1964e0071566e89cc79fd529a7
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-31 11:12:04 +0900

    🗳️::ConfigurationPropertiesConfig

--- hexagonal-infrastructure/src/main/kotlin/com/example/global/config/property/ConfigurationPropertiesConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.example.global.config.property
-
-import com.example.global.config.security.SecurityProperties
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.context.annotation.Configuration
-
-@ConfigurationPropertiesScan(basePackageClasses = [SecurityProperties::class])
-@Configuration
-class ConfigurationPropertiesConfig
