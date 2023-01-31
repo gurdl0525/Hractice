bc2b7c20ecc7964559d869fde8684e6e6a5fb868
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-01 00:42:27 +0900

    🔧::Refactoring

--- hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/UserNotFoundException.kt
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/UserNotFoundException.kt
@@ -4,6 +4,7 @@ import com.example.global.error.BusinessException
 import com.example.global.error.data.GlobalErrorCode
 
 class UserNotFoundException(): BusinessException(GlobalErrorCode.USER_NOT_FOUND) {
+
     companion object {
         @JvmField
         val EXCEPTION = UserNotFoundException()
