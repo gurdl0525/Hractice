041ad0ac1f5a341b8da14cb9570b16bb8357824e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-01 00:42:12 +0900

    🔧::Refactoring

--- hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/InternalServerErrorException.kt
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/InternalServerErrorException.kt
@@ -4,6 +4,7 @@ import com.example.global.error.BusinessException
 import com.example.global.error.data.GlobalErrorCode
 
 class InternalServerErrorException(): BusinessException(GlobalErrorCode.INTERNAL_SERVER_ERROR) {
+
     companion object {
         @JvmField
         val EXCEPTION = InternalServerErrorException()
