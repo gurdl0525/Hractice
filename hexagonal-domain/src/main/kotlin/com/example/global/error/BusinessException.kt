e48a586dee495caa909c884b01f72340046e7b89
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-02-01 00:41:59 +0900

    🔧::Refactoring

--- hexagonal-domain/src/main/kotlin/com/example/global/error/BusinessException.kt
+++ hexagonal-domain/src/main/kotlin/com/example/global/error/BusinessException.kt
@@ -3,5 +3,6 @@ package com.example.global.error
 abstract class BusinessException(
     open val customErrorProperty: CustomErrorProperty
 ): RuntimeException() {
+
     override fun fillInStackTrace() = this
 }
