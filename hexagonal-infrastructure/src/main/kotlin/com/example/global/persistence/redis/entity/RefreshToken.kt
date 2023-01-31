5bb991a096ae1d87f17a08f4a3c3cc91735eb1a2
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-31 11:08:19 +0900

    📌::RefreshTokenEntity

---
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/persistence/redis/entity/RefreshToken.kt
@@ -0,0 +1,15 @@
+package com.example.global.persistence.redis.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
+
+@RedisHash(value = "RefreshToken", timeToLive = 60 * 60 * 24 * 7)
+data class RefreshToken (
+
+    @Id
+    val accountId: String,
+
+    @Indexed
+    val token: String
+)
