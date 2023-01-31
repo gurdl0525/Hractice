b881eedb6b29ab8a354edab292e187430aa9348f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-31 11:06:50 +0900

    📌::JwtTokenFilter

---
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/config/filter/JwtTokenFilter.kt
@@ -0,0 +1,22 @@
+package com.example.global.config.filter
+
+import com.example.global.config.security.jwt.JwtTokenResolver
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtTokenFilter(
+    private val jwtTokenResolver: JwtTokenResolver
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        jwtTokenResolver.resolveToken(request)?.run {
+
+        }
+    }
+
+}
