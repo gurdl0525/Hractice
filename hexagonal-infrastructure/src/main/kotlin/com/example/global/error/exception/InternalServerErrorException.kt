4a304efe885df5c625fc1747869cb29dc2f78f78
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-01-31 11:06:25 +0900

    📌::InternalServerErrorException

---
+++ hexagonal-infrastructure/src/main/kotlin/com/example/global/error/exception/InternalServerErrorException.kt
@@ -0,0 +1,11 @@
+package com.example.global.error.exception
+
+import com.example.global.error.BusinessException
+import com.example.global.error.data.GlobalErrorCode
+
+class InternalServerErrorException(): BusinessException(GlobalErrorCode.INTERNAL_SERVER_ERROR) {
+    companion object {
+        @JvmField
+        val EXCEPTION = InternalServerErrorException()
+    }
+}
